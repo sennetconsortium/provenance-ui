7c283a1672b69af11e23850aaf98b277400bb718
Lisa-Ann B <lisabruney@pitt.edu>
2022-11-14 13:15:47 -0500

    Update dist for Legend

--- dist/components/Legend.js
+++ dist/components/Legend.js
@@ -5,10 +5,12 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 require("core-js/modules/web.dom-collections.iterator.js");
-var _react = require("react");
+var _react = _interopRequireWildcard(require("react"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
 var _jquery = _interopRequireDefault(require("jquery"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 const Legend = _ref => {
   let {
     colorMap,
@@ -64,22 +66,22 @@ const Legend = _ref => {
   const buildLegend = () => {
     let result = [];
     for (let type in colors) {
-      result.push( /*#__PURE__*/React.createElement("li", {
+      result.push( /*#__PURE__*/_react.default.createElement("li", {
         className: "c-legend__item js-legend__item",
         key: "legend--".concat(type),
         "data-node": type
-      }, /*#__PURE__*/React.createElement("span", {
+      }, /*#__PURE__*/_react.default.createElement("span", {
         className: "c-legend__color c-legend__color--".concat(type),
         style: {
           backgroundColor: colors[type]
         }
-      }), /*#__PURE__*/React.createElement("span", {
+      }), /*#__PURE__*/_react.default.createElement("span", {
         className: "c-legend__label"
-      }, /*#__PURE__*/React.createElement("span", null, type))));
+      }, /*#__PURE__*/_react.default.createElement("span", null, type))));
     }
     return result;
   };
-  return /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/_react.default.createElement("div", {
     className: "c-legend ".concat(filterable ? 'c-legend--filterable' : '')
   }, buildLegend());
 };
--- src/lib/components/Legend.jsx
+++ src/lib/components/Legend.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState, useRef } from 'react'
+import React, { useContext, useEffect, useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 import $ from 'jquery'
 
