d5b683b84624abd9140e346e472161dcf37e31c7
Lisa-Ann B <lisabruney@pitt.edu>
2022-11-17 14:02:26 -0500

    Move common methods to DataConverter class

---
+++ src/lib/js/DataConverter.js
@@ -0,0 +1,185 @@
+class DataConverter {
+    constructor(data, map) {
+        this.data = data
+        this.map = map
+        this.relationships = []
+        this.nodes = []
+        this.error = null
+    }
+
+    isActivity(item) {
+        return item.isActivity || (this.keys && this.keys.type ? item[this.keys.type] === 'Activity' : false)
+    }
+
+    /**
+     * Returns a parent entity type from provided list object
+     * @param id
+     * @returns {*|string}
+     */
+    getParentEntityTypeFromId(id) {
+        const rootKeys = Object.assign({}, ...Object.entries(this.map.root).map(([a,b]) => ({ [b]: a })))
+        try {
+            const type = this.list[id] ? this.list[id][rootKeys.labels] : ''
+            return type
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    /**
+     * Returns a property name from the map.root
+     * @param key {string}
+     * @returns {string}
+     */
+    getPropFromMap(key = 'id') {
+        let result = '_id'
+        for (let prop in this.map.root) {
+            if (this.map.root[prop] === key ) {
+                return prop
+            }
+        }
+        return result
+    }
+
+    formatNode(item) {
+        let data = {}
+        let type;
+
+        // Capture properties wanted for
+        for (let prop in item) {
+            let value = item[this.map.root[prop]] !== undefined ? item[this.map.root[prop]] : item[prop];
+            if (this.map.root[prop]) {
+                if (this.map.root[prop] === 'labels') {
+                    data.labels = item.labels || [value]
+                    type = value;
+                } else if (prop === this.map.actor.dataProp && this.isActivity(item) ) {
+                    data.properties = {
+                        [this.map.actor.visualProp]: item[this.map.actor.dataProp]
+                    }
+                    data.text = this.evaluateCallbackOnValue(prop, value)
+                } else if (this.map.root[prop] === 'text') {
+                    data.text = this.isActivity(item) ? this.evaluateCallbackOnValue(prop, value) : item[this.getPropFromMap('labels')]
+                }  else {
+                    data[this.map.root[prop]] = this.evaluateCallbackOnValue(prop, value)
+                }
+            }
+        }
+        data.properties = data.properties || {}
+        for (let gProp of this.map.props) {
+            data.properties[gProp] = item[gProp]
+        }
+
+        if (type && typeof this.map.typeProps[type] === 'object') {
+            for (let tProp of this.map.typeProps[type]) {
+                data.properties[tProp] = this.evaluateCallbackOnValue(tProp, item[tProp])
+            }
+        }
+        data.parentType = this.getParentEntityTypeFromId(item.parentId)
+        this.nodes.push(data)
+    }
+
+    /**
+     * Returns a particular node as a highlight
+     * @param prop {string}
+     * @param index {int}
+     * @returns {{property: string, class: string, value: string}}
+     */
+    getNodeAsHighlight(prop, index = 0) {
+        const node = this.nodes[index]
+        return {
+            'class': node.labels[0],
+            property: prop,
+            value: node.properties[prop]
+        }
+    }
+
+    /**
+     * Runs a callback on a given value
+     * @param cb {function}
+     * @param val {string}
+     * @returns {string}
+     */
+    valueCallback(cb, val) {
+        if (typeof cb === 'string') {
+            if (typeof this[cb]  === 'function') {
+                return this[cb](val)
+            }
+            return val;
+        } else {
+            try {
+                return cb(val)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        return val
+    }
+
+    /**
+     * Formats a date.
+     * @param val
+     * @returns {string}
+     */
+    formatDate(val) {
+        return new Date(val * 1000).toLocaleString()
+    }
+
+    /**
+     * Formats a given name into "Last name, F." initial format
+     * @param val
+     * @returns {string|*}
+     */
+    lastNameFirstInitial(val) {
+        let name = val.split(' ')
+        return name.length > 1 ? `${name[1]}, ${name[0][0]}.` : val
+    }
+
+    /**
+     * Determines if a callback should be run.
+     * @param prop {string}
+     * @param value
+     * @returns {string|*}
+     */
+    evaluateCallbackOnValue(prop, value) {
+        return this.map.callbacks[prop]
+            ? this.valueCallback(this.map.callbacks[prop], value) : value;
+    }
+
+    /**
+     * Returns all node
+     * @returns {[]}
+     */
+    getNodes() {
+        return this.nodes;
+    }
+
+    /**
+     * Return all relationships
+     * @returns {[]}
+     */
+    getRelationships() {
+        return this.relationships;
+    }
+
+    /**
+     * Return neo4jFormatted data
+     * @param data
+     * @returns {{results: [{data: [{graph: {relationships: *, nodes: *}}], columns: []}], errors: *[]}}
+     */
+    getNeo4jFormat(data) {
+        return {
+            results: [{
+                columns: data.columns,
+                data: [{
+                    graph: {
+                        nodes: data.nodes,
+                        relationships: data.relationships
+                    }
+                }]
+            }],
+            errors: []
+        }
+    }
+}
+
+export default DataConverter
--- src/lib/js/generic/DataConverterGeneric.js
+++ src/lib/js/generic/DataConverterGeneric.js
@@ -1,4 +1,5 @@
 import GraphGeneric from './GraphGeneric'
+import DataConverter from '../DataConverter'
 
 /**
  * Converts any data object to neo4j formatted data for the visualization.
@@ -35,101 +36,11 @@ import GraphGeneric from './GraphGeneric'
  *     }
  * }
  */
-class DataConverterGeneric {
-    constructor(rawData, map, list) {
-        this.rawNodes = rawData;
-        this.relationships = []
-        this.nodes = []
-        this.map = map
+class DataConverterGeneric extends DataConverter {
+    constructor(data, map, list) {
+        super(data, map)
         this.error = null
         this.list = list || {}
-    }
-
-    /**
-     * Formats a date.
-     * @param val
-     * @returns {string}
-     */
-    formatDate(val) {
-        return new Date(val * 1000).toLocaleString()
-    }
-
-    /**
-     * Formats a given name into "Last name, F." initial format
-     * @param val
-     * @returns {string|*}
-     */
-    lastNameFirstInitial(val) {
-        let name = val.split(' ')
-        return name.length > 1 ? `${name[1]}, ${name[0][0]}.` : val
-    }
-
-    /**
-     * Returns a parent entity type from provided list object
-     * @param id
-     * @returns {*|string}
-     */
-    getParentEntityTypeFromId(id) {
-        const rootKeys = Object.assign({}, ...Object.entries(this.map.root).map(([a,b]) => ({ [b]: a })))
-        try {
-            const type = this.list[id] ? this.list[id][rootKeys.labels] : ''
-            return type
-        } catch (e) {
-            console.error(e)
-        }
-
-    }
-
-    /**
-     * Runs a callback on a given value
-     * @param cb {function}
-     * @param val {string}
-     * @returns {string}
-     */
-    valueCallback(cb, val) {
-        if (typeof cb === 'string') {
-            if (typeof this[cb]  === 'function') {
-                return this[cb](val)
-            }
-            return val;
-        } else {
-            try {
-                return cb(val)
-            } catch (e) {
-                console.log(e)
-            }
-        }
-        return val
-    }
-
-    /**
-     * Returns a particular node as a highlight
-     * @param prop {string}
-     * @param index {int}
-     * @returns {{property: string, class: string, value: string}}
-     */
-    getNodeAsHighlight(prop, index = 0) {
-        const node = this.nodes[index]
-        return {
-            'class': node.labels[0],
-            property: prop,
-            value: node.properties[prop]
-        }
-    }
-
-    /**
-     * Returns a property name from the map.root
-     * @param key {string}
-     * @returns {string}
-     */
-    getPropFromMap(key = 'id') {
-        let result = '_id'
-        for (let prop in this.map.root) {
-            if (this.map.root[prop] === key ) {
-                return prop
-            }
-        }
-        return result
     }
 
     /**
@@ -139,7 +50,7 @@ class DataConverterGeneric {
         let i = 0;
         const idProp = this.getPropFromMap()
         const actorProp = this.map.actor.dataProp
-        for (let item of this.rawNodes) {
+        for (let item of this.data) {
             if (item.endNode) {
                 this.relationships.push({
                     id: i,
@@ -161,20 +72,9 @@ class DataConverterGeneric {
     runFormatting(nodes) {
         let graph = new GraphGeneric()
         graph.dfs(nodes)
-        this.rawNodes = graph.getResult()
+        this.data = graph.getResult()
         this.reformatNodes()
         this.reformatRelationships()
-    }
-
-    /**
-     * Determines if a callback should be run.
-     * @param prop {string}
-     * @param value
-     * @returns {string|*}
-     */
-    evaluateCallbackOnValue(prop, value) {
-        return this.map.callbacks[prop]
-            ? this.valueCallback(this.map.callbacks[prop], value) : value;
     }
 
     /**
@@ -183,41 +83,8 @@ class DataConverterGeneric {
      */
     reformatNodes() {
         try {
-            for (let item of this.rawNodes) {
-                let data = {}
-                let type;
-
-                // Capture properties wanted for
-                for (let prop in item) {
-                    let value = item[this.map.root[prop]] !== undefined ? item[this.map.root[prop]] : item[prop];
-                    if (this.map.root[prop]) {
-                        if (this.map.root[prop] === 'labels') {
-                            data.labels = item.labels || [value]
-                            type = value;
-                        } else if (prop === this.map.actor.dataProp && item.isActivity ) {
-                            data.properties = {
-                                [this.map.actor.visualProp]: item[this.map.actor.dataProp]
-                            }
-                            data.text = this.evaluateCallbackOnValue(prop, value)
-                        } else if (this.map.root[prop] === 'text') {
-                            data.text = item.isActivity ? this.evaluateCallbackOnValue(prop, value) : item[this.getPropFromMap('labels')]
-                        }  else {
-                            data[this.map.root[prop]] = this.evaluateCallbackOnValue(prop, value)
-                        }
-                    }
-                }
-                data.properties = data.properties || {}
-                for (let gProp of this.map.props) {
-                    data.properties[gProp] = item[gProp]
-                }
-
-                if (type && typeof this.map.typeProps[type] === 'object') {
-                    for (let tProp of this.map.typeProps[type]) {
-                        data.properties[tProp] = this.evaluateCallbackOnValue(tProp, item[tProp])
-                    }
-                }
-                data.parentType = this.getParentEntityTypeFromId(item.parentId)
-                this.nodes.push(data)
+            for (let item of this.data) {
+                this.formatNode(item)
             }
         } catch (e) {
             this.error = e
@@ -226,41 +93,6 @@ class DataConverterGeneric {
         return this;
     }
 
-    /**
-     * Returns all node
-     * @returns {[]}
-     */
-    getNodes() {
-        return this.nodes;
-    }
-
-    /**
-     * Return all relationships
-     * @returns {[]}
-     */
-    getRelationships() {
-        return this.relationships;
-    }
-
-    /**
-     * Return neo4jFormatted data
-     * @param data
-     * @returns {{results: [{data: [{graph: {relationships: *, nodes: *}}], columns: []}], errors: *[]}}
-     */
-    getNeo4jFormat(data) {
-        return {
-            results: [{
-                columns: data.columns,
-                data: [{
-                    graph: {
-                        nodes: data.nodes,
-                        relationships: data.relationships
-                    }
-                }]
-            }],
-            errors: []
-        }
-    }
 }
 
 export default DataConverterGeneric
