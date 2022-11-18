cbc4ce06a63c51757493d7d7857c76723c5c1518
Lisa-Ann B <lisabruney@pitt.edu>
2022-11-18 16:28:31 -0500

    Wrote basic functionality for toggle (not done)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import AppContext from './context/AppContext'
 import ProvenanceUI from './lib/components/ProvenanceUI'
 import Legend from './lib/components/Legend'
+import Toggle from './lib/components/Toggle'
 import sample from './data/sample'
 
 function App() {
-    const { contextData, options, loading } = useContext(AppContext)
+    const { contextData, options, loading, setContextData } = useContext(AppContext)
+    const [toggle, setToggle] = useState(false)
+
+    const toggleData = (data) => {
+        setToggle(true)
+        setContextData(data)
+    }
 
     return (
         <div className={`c-App`}>
             { loading && <ProvenanceUI data={sample} /> }
-            { !loading && contextData && <ProvenanceUI data={contextData} options={ options } /> }
+            { !loading && contextData && toggle && <ProvenanceUI data={contextData} options={ options } /> }
             { options.colorMap && <Legend colorMap={options.colorMap} /> }
+            { !loading && contextData && <Toggle data={contextData} context={ toggleData } /> }
         </div>
     )
 }
