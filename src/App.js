720ab19910a6c696289744716dee262d4d0d9d0a
Lisa-Ann B <lisabruney@pitt.edu>
2022-11-21 13:01:57 -0500

    Add working toggle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import AppContext from './context/AppContext'
 import ProvenanceUI from './lib/components/ProvenanceUI'
 import Legend from './lib/components/Legend'
@@ -6,20 +6,30 @@ import Toggle from './lib/components/Toggle'
 import sample from './data/sample'
 
 function App() {
-    const { contextData, options, loading, setContextData } = useContext(AppContext)
-    const [toggle, setToggle] = useState(false)
+    const { contextData, options, loading } = useContext(AppContext)
+    const [data, setData] = useState(null)
 
-    const toggleData = (data) => {
-        setToggle(true)
-        setContextData(data)
+    useEffect(() => {
+        setData(contextData)
+    })
+
+    const toggleData = (data, isFiltered) => {
+        setData(data)
+        const ui = window.ProvenanceUId3
+        const charge = ui.options.simulation.charge
+        ui.setCharge(isFiltered ? charge * 4 : charge)
+        ui.setTransition(true)
+        ui.clearCanvas()
+        ui.updateWithNeo4jData(data)
     }
 
     return (
         <div className={`c-App`}>
-            { loading && <ProvenanceUI data={sample} /> }
-            { !loading && contextData && toggle && <ProvenanceUI data={contextData} options={ options } /> }
+           
+            {/* { loading && <ProvenanceUI data={sample} /> } */}
+            { !loading && data && <ProvenanceUI data={data} options={ options } /> }
             { options.colorMap && <Legend colorMap={options.colorMap} /> }
-            { !loading && contextData && <Toggle data={contextData} context={ toggleData } /> }
+            { !loading && data && <Toggle data={data} context={ toggleData } /> }
         </div>
     )
 }
