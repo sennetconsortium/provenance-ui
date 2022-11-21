225b4eb4fa6a76cf8139bfa4dc8688aa63b4849e
Lisa-Ann B <lisabruney@pitt.edu>
2022-11-21 12:36:34 -0500

    Update toggle functionality

--- src/lib/components/Toggle.jsx
+++ src/lib/components/Toggle.jsx
@@ -1,22 +1,43 @@
 import React from 'react'
 import NodeToggle from '../js/NodeToggle'
+import log from 'loglevel'
 import $ from 'jquery'
 
-function Toggle({ context, data}) {
+function Toggle({ context, data, label = 'Toggle', className = 'btn'}) {
     const toggleData = (e) => {
+        const provenanceData = window.provenanceData
+        if (provenanceData && provenanceData.all) {
+            data = provenanceData.all
+        } else {
+            console.log('The data, the data')
+            window.provenanceData = {
+                all: JSON.parse(JSON.stringify(data))
+            }
+        }
+        log.debug('Before toggle', data.results[0].data[0].graph)
         const $el =  $(e.currentTarget)
         const className = 'hide-activities'
         $el.toggleClass(className)
+        const _data = Object.assign({}, data)
+
         if ($el.hasClass(className)) {
-           const result = NodeToggle(data.results[0].data[0].graph)
-           console.log('After toogle', result)
-           data.results[0].data[0].graph = result
-           context(data)
-        
+            if (window.provenanceData.filtered) {
+                context(window.provenanceData.filtered, true)
+            } else {
+                const result = NodeToggle(_data.results[0].data[0].graph)
+                log.debug('After toggle', result)
+                _data.results[0].data[0].graph = result
+                window.provenanceData.filtered = {... _data }
+                context(_data, true)
+            }
+            
+        } else {
+            log.debug('Fixed After toggle', _data.results[0].data[0].graph)
+            context(_data, false)
         }
     }
     return (
-        <button onClick={toggleData}>Toggle</button>
+        <button className={className} onClick={toggleData}><span>{label}</span></button>
     )
 }
 
