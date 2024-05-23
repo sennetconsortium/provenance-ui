a3a3b71c210a6a79aef98404ef59305f81a96358
Lisa-Ann B <lisabruney@pitt.edu>
2024-05-23 16:58:37 -0400

    Add check on graphDepth

--- src/lib/js/ProvenanceTree.js
+++ src/lib/js/ProvenanceTree.js
@@ -60,7 +60,7 @@ function ProvenanceTree(d3, selector, _options) {
     let isInit = true
     const data = {}
     const options = {
-        graphDepth: null,
+        graphDepth: 0,
         zoomActivated: false,
         colorMap: {
             Sample: "#ebb5c8",
@@ -644,14 +644,18 @@ function ProvenanceTree(d3, selector, _options) {
     }
 
     function buildNodes() {
+        options.graphDepth = 0
         const getDepth = (d) => {
             return d.depth
         }
         const childInfo = (d, i) => {
             const posY = (ci, mod) => {
                 const k = (mod ? 12 : 20)
-                options.graphDepth = (ci * k * d.depth)
-                return options.graphDepth
+                const gDepth = (ci * k * d.depth)
+                if (gDepth > options.graphDepth) {
+                    options.graphDepth = gDepth
+                }
+                return gDepth
             }
             treeWidth = Math.max(treeWidth, (d.children ? d.children.length : 0))
             if (d.parent) {
